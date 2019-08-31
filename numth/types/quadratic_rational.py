15a8bf5af5d6843cdf810d655b9e70de9fac35c3
itscomputers <its_computers@protonmail.com>
2019-08-30 18:28:54 -0600

    start quadratic rational type

--- numth/types/quadratic_rational.py
+++ numth/types/quadratic_rational.py
@@ -4,6 +4,7 @@ import math
 import numbers
 import operator
 
+from ..basic import gcd, lcm
 from .rational import frac
 #===========================================================
 
@@ -11,39 +12,51 @@ class QuadraticRational(numbers.Number):
     """
     Quadratic rational elements.
     """
-    def __init__(self, real, imag, root):
-        self.real = frac(real)
-        self.imag = frac(imag)
-        self.root = frac(root)
+    def __init__(self, real, imag, denom, root, _normalize=True):
+        self._real = int(real)
+        self._imag = int(imag)
+        self._denom = int(denom)
+        self.root = int(root)
+        if _normalize:
+            d = gcd(real, imag, denom)
+            if self._denom < 0:
+                d = -d
+            self._real //= d
+            self._imag //= d
+            self._denom //= d
+
+    @property
+    def real(self):
+        return frac(self._real, self._denom)
+
+    @property
+    def imag(self):
+        return frac(self._imag, self._denom)
+
+    @property
+    def components(self):
+        return (self._real, self._imag, self._denom)
+
+    @property
+    def signature(self):
+        return (*self.components, self.root)
 
     #=========================
 
     def __repr__(self):
         if self.root == -1:
             root_disp = ' \u2139'
-        elif self.root.denom == 1:
-            root_disp = ' \u221a{}'.format(self.root)
         else:
-            root_disp = ' \u221a({})'.format(self.root)
+            root_disp = ' \u221a{}'.format(self.root)
 
-        representation = ' + '.join(
-            ''.join(format(y) for y in x) \
-            for x in zip(self.components, ('', root_disp))
-        )
+        representation = '( ' + ' + '.join([
+            format(self._real),
+            '{}{}'.format(self._imag, root_disp)
+        ]) + ' ) / {}'.format(self._denom)
 
         return representation.replace(' + -', ' - ')
-    
+
     #=========================
-
-    def component_map(self, function, other=None):
-        if other is None:
-            return QuadraticRational(*map(function, self.components), self.root)
-        return QuadraticRational(
-            *map(function, self.components, other.components),
-            self.root
-        )
-
-    #-------------------------
 
     def is_compatible(self, other):
         return isinstance(other, QuadraticRational) and self.root == other.root
@@ -51,34 +64,51 @@ class QuadraticRational(numbers.Number):
     #=========================
 
     @property
-    def signature(self):
-        return (self.real, self.imag, self.root)
-
-    @property
-    def components(self):
-        return (self.real, self.imag)
+    def _norm(self):
+        return self._real**2 - self.root * self._imag**2
 
     @property
     def norm(self):
-        return self.real**2 - self.root * self.imag**2
+        return frac(self._norm, self._denom**2)
 
     @property
     def conjugate(self):
-        return QuadraticRational(self.real, -self.imag, self.root)
+        return QuadraticRational(
+            self._real,
+            -self._imag,
+            self._denom,
+            self.root,
+            _normalize=False
+        )
 
     @property
     def inverse(self):
-        return self.conjugate.component_map(self.norm.inverse().__mul__)
+        return QuadraticRational(
+            self._real * self._denom,
+            -self._imag * self._denom,
+            self._norm,
+            self.root
+        )
 
     #=========================
 
     @classmethod
     def from_real(self, real, root):
-        return QuadraticRational(frac(real), 0, root)
+        real = frac(real)
+        return QuadraticRational(real.numer, 0, real.denom, root, _normalize=False)
 
     @classmethod
     def from_complex(self, cplx):
-        return QuadraticRational(frac(cplx.real), frac(cplx.imag), -1)
+        real = frac(cplx.real)
+        imag = frac(cplx.imag)
+        denom = lcm(real.denom, imag.denom)
+        return QuadraticRational(
+            real.numer * denom // real.denom,
+            imag.numer * denom // imag.denom,
+            denom,
+            -1,
+            _normalize=False
+        )
 
     #=========================
 
@@ -100,9 +130,6 @@ class QuadraticRational(numbers.Number):
         if self.is_compatible(other):
             return self.norm < other.norm
 
-        if isinstance(other, numbers.Real):
-            return self < QuadraticRational.from_real(other, self.root)
-
         if isinstance(other, numbers.Complex) and self.root == -1:
             return self < QuadraticRational.from_complex(other)
 
@@ -113,9 +140,6 @@ class QuadraticRational(numbers.Number):
     def __gt__(self, other):
         if self.is_compatible(other):
             return self.norm > other.norm
-
-        if isinstance(other, numbers.Real):
-            return self > QuadraticRational.from_real(other, self.root)
 
         if isinstance(other, numbers.Complex) and self.root == -1:
             return self > QuadraticRational.from_complex(other)
@@ -138,16 +162,39 @@ class QuadraticRational(numbers.Number):
         return self
 
     def __neg__(self):
-        return self.component_map(operator.__neg__)
+        return QuadraticRational(
+            -self._real,
+            -self._imag,
+            self._denom,
+            self.root,
+            _normalize=False
+        )
 
     #=========================
 
     def __add__(self, other):
         if self.is_compatible(other):
-            return self.component_map(operator.__add__, other)
+            real = self._real * other._denom + other._real * self._denom
+            imag = self._imag * other._denom + other._imag * self._denom
+            denom = self._denom * other._denom
+            return QuadraticRational(real, imag, denom, self.root)
+
+        if isinstance(other, int):
+            return QuadraticRational(
+                self._real + self._denom * other,
+                self._imag,
+                self._denom,
+                self.root,
+                _normalize=False
+            )
 
         if isinstance(other, numbers.Real):
-            return QuadraticRational(self.real + frac(other), self.imag, self.root)
+            o = frac(other)
+            real = self._real * o.denom + self._denom * o.numer
+            imag = self._imag * o.denom
+            denom = self._denom * o.denom
+            return QuadraticRational(real, imag, denom, self.root)
+
 
         if isinstance(other, numbers.Complex) and self.root == -1:
             return self + QuadraticRational.from_complex(other)
@@ -163,15 +210,42 @@ class QuadraticRational(numbers.Number):
 
     def __mul__(self, other):
         if self.is_compatible(other):
-            real = (self.real * other.real) + (self.imag * other.imag * self.root)
-            imag = (self.real * other.imag) + (self.imag * other.real)
-            return QuadraticRational(real, imag, self.root)
+            real = self._real * other._real \
+                    + self._imag * other._imag * self.root
+            imag = self._real * other._imag \
+                    + self._imag * other._real
+            denom = self._denom * other._denom
+            return QuadraticRational(real, imag, denom, self.root)
+
+        if isinstance(other, int):
+            d = gcd(other, self._denom)
+            m = other // d
+            real = self._real * m
+            imag = self._imag * m
+            denom = self._denom // d
+            return QuadraticRational(
+                self._real * m,
+                self._imag * m,
+                self._denom // d,
+                self.root,
+                _normalize=False
+            )
 
         if isinstance(other, numbers.Real):
-            return self.component_map(frac(other).__mul__)
+            o = frac(other)
+            d = gcd(o.numer, self._denom)
+            m = o.numer // d
+            e = gcd(o.denom, self._real, self._imag)
+            return QuadraticRational(
+                self._real // e * m,
+                self._imag // e * m,
+                self._denom // d * o.denom // e,
+                self.root,
+                _normalize=False
+            )
 
         if isinstance(other, numbers.Complex) and self.root == -1:
-            return self + QuadraticRational.from_complex(other)
+            return self * QuadraticRational.from_complex(other)
 
         return NotImplemented
 
@@ -182,7 +256,7 @@ class QuadraticRational(numbers.Number):
             return self * other.inverse
 
         if isinstance(other, numbers.Real):
-            return self.component_map(frac(other).__rtruediv__)
+            return self * (frac(other).inverse)
 
         if isinstance(other, numbers.Complex) and self.root == 1:
             return self / QuadraticRational.from_complex(other)
