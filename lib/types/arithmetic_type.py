f2652e01a30c36ab8f9bf79d352e6d22e0d2ad6d
itscomputers <its_computers@protonmail.com>
2019-11-30 09:57:47 -0700

    update docs on general arithmetic type class

--- lib/types/arithmetic_type.py
+++ lib/types/arithmetic_type.py
@@ -1,11 +1,41 @@
-#   numth/types/arithmetic_type.py
+#   lib/types/arithmetic_type.py
+#   - class for common arithmetic functionality of inheriting types
+
+#===========================================================
+__all__ = [
+    'ArithmeticType'
+]
 #===========================================================
 
 
 class ArithmeticType:
 
+    """
+    This class is not meant to be instantiated directly.  Its purpose is to
+    meta-program away some of the common functionality of arithmetic types.
+
+    Suppose we have two number types `TypeA` and `TypeB` that are arithmetically
+    compatible in some way.  Some examples of how to implement arithmetic.
+    - To compare `TypeA` instances, we implement the method
+        `_eq_TypeA` on `TypeA` class.
+    - To compare an instance of `TypeA` to `TypeB`, we implement the method
+        `_eq_TypeB` on `TypeA` class.
+    - to add instances of `TypeA`, we implement the method
+        `_add_TypeA` on `TypeA` class.
+    - To add `type_a_instance + type_b_instance`, we implement the method
+        `_add_TypeB` on `TypeA` class.
+    - To subtract, we implement `__neg__` and appropriate `_add_<type>` method.
+
+    Note that addition is assumed to be commutative, so `__radd__` is always
+    defined.  Also, assuming `__neg__` is defined, `__rsub__` is assumed to
+    operate as usual.  However, multiplication may not be commutative, so
+    `__rmul__` is left to be implemented as above on a particular class.
+
+    See `lib.types.quadratic` and `lib.types.quadratic_integer` for examples.
+    """
 
     def execute(self, operation, other, *args):
+        """Execute, if possible, an operation between self and other."""
         method_name = '_{}_{}'.format(operation, other.__class__.__name__)
         if hasattr(self, method_name):
             return eval('self.{}'.format(method_name))(other, *args)
@@ -80,6 +110,10 @@ class ArithmeticType:
     #=========================
 
     def _pow(self, other):
+        """
+        Compute self**other.  To use, must implement `_zero_pow_int`.
+        For negative exponents, must implement `_inv_pow_int`.
+        """
         if other < 0:
             return self.execute('inv_pow', other)
 
@@ -95,6 +129,10 @@ class ArithmeticType:
         return self * (self * self)._pow(other // 2)
 
     def _pow_mod(self, other, modulus):
+        """
+        Compute self**other % modulus.  To use, must implement `_zero_pow_int`.
+        For negative exponents, must implement `_inv_pow_mod_int`.
+        """
         if other < 0:
             return self.execute('inv_pow_mod', other, modulus)
 
