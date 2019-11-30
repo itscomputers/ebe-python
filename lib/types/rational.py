09f1e27ebbaddb3dc93b463116a386c61e4fc94f
itscomputers <its_computers@protonmail.com>
2019-11-30 12:05:24 -0700

    update docs for rational type

--- lib/types/rational.py
+++ lib/types/rational.py
@@ -1,4 +1,6 @@
-#   numth/types/rational.py
+#   lib/types/rational.py
+#   - class for arithmetic of rational numbers
+
 #===========================================================
 from fractions import Fraction
 import math
@@ -6,6 +8,11 @@ import numbers
 
 from ..config import default
 from ..basic import gcd, integer_sqrt, is_square
+#===========================================================
+__all__ = [
+    'frac',
+    'Rational',
+]
 #===========================================================
 
 def frac(*inputs):
@@ -24,12 +31,24 @@ def frac(*inputs):
 
 #===========================================================
 
-class Rational(Fraction):
-    """
-    Rational number class.
 
-    Primarily inherits from fractions.Fraction
+class Rational(Fraction):
+
     """
+    Class that represents `numer / denom` as a rational number.
+
+    This class inherits much of its functionality from `fractions.Fraction`.
+    However, it implements some opinionated differences and some additional
+    functionality.  For instance, Fraction allows operations with floats,
+    but Rational does not, since floats cannot be trusted in general to be
+    accurate.  A Rational can be built from a float, in which case operations
+    may then occur, but this enforces intentional float to Rational conversion.
+
+    + numer: int
+    + denom: int
+    + _normalize: bool --whether to check if `numer, denom` are in lowest terms
+    """
+
     def __new__(cls, numer, denom, _normalize=True):
         self = super(Rational, cls).__new__(cls)
 
@@ -59,18 +78,22 @@ class Rational(Fraction):
 
     @property
     def to_fraction(self):
+        """Cast to Fraction."""
         return Fraction(self.numer, self.denom, _normalize=False)
 
     @classmethod
     def from_int(self, integer):
+        """Build from int."""
         return Rational(integer, 1, _normalize=False)
 
     @classmethod
     def from_Rational(self, rational):
+        """Build from Rational."""
         return rational
 
     @classmethod
     def from_Fraction(self, fraction):
+        """Build from Fraction."""
         return Rational(
             fraction._numerator,
             fraction._denominator,
@@ -79,18 +102,22 @@ class Rational(Fraction):
 
     @classmethod
     def from_float(self, fl):
+        """Build from float."""
         return Rational.from_str(str(fl))
 
     @classmethod
     def from_str(self, string):
+        """Build from str."""
         return Rational.from_Fraction(Fraction(string.replace(' ', '')))
 
     @classmethod
     def from_list(self, li):
+        """Build from List[int]."""
         return Rational(*li)
 
     @classmethod
     def from_tuple(self, tu):
+        """Build from Tuple[int]."""
         return Rational(*tu)
 
     #-------------------------
@@ -118,12 +145,8 @@ class Rational(Fraction):
         """
         Express rational number as a string in decimal form.
 
-        params
-        + num_digits : int
-
-        return
-        str
-            decimal representation of self
+        + num_digits: int
+        ~> str
         """
         if num_digits == 0:
             return str(self.round_to_nearest_int)
@@ -144,38 +167,62 @@ class Rational(Fraction):
     #=========================
 
     @property
-    def inverse(self):
+    def reciprocal(self):
+        """
+        Compute reciprocal rational number.
+
+        ~> Rational
+        """
         if self._numerator < 0:
             return Rational(-self._denominator, -self._numerator, _normalize=False)
         return Rational(self._denominator, self._numerator, _normalize=False)
 
     @property
+    def inverse(self):
+        """
+        Compute multiplicative inverse.
+
+        ~> Rational
+        """
+        return self.reciprocal
+
+    @property
     def round_to_nearest_int(self):
+        """
+        Compute nearest integer, rounding up if halfway between.
+
+        ~> int
+        """
         return math.floor(self + Rational(1, 2))
 
     @property
     def round_prefer_toward_zero(self):
+        """
+        Compute nearest integer, rounding toward zero if halfway between.
+
+        ~> int
+        """
         return int(self) if self.denom == 2 else self.round_to_nearest_int
 
     @property
     def is_square(self):
+        """
+        Determine if is a square number.
+
+        ~> bool
+        """
         return is_square(self._numerator) and is_square(self._denominator)
 
     #=========================
 
     def approx_equal(self, other, num_digits=None):
         """
-        Approximate equality of two rational numbers.
+        Determine if approximately equal to `other`,
+        ie, if ``abs(self - other) < 10**(-num_digits)``.
 
-        Determines if ``abs(self - other) < 10**(-num_digits)``.
-
-        params
-        + other : Rational
-            or type that can be converted to Rational using frac
-        + num_digits : int
-
-        return
-        bool
+        + other: Union[int, float, string, Rational]
+        + num_digits: int
+        ~> bool
         """
         if num_digits == 0:
             return abs(self - other) < 1
@@ -193,16 +240,10 @@ class Rational(Fraction):
 
     def sqrt(self, num_digits=None):
         """
-        Square root (or approximation of square root) of a rational number.
+        Compute square root or approximation of square root.
 
-        Computes sqrt such that ``sqrt**2 == self``,
-        or up to desired accuracy if not a perfect square.
-
-        params
-        + num_digits : int
-
-        return
-        Rational
+        + num_digits: int
+        ~> Rational
         """
         if self < 0:
             raise ValueError('Cannot take square root of negative number')
@@ -233,16 +274,10 @@ class Rational(Fraction):
 
     def inverse_sqrt(self, num_digits=None):
         """
-        Inverse square root (or approximation) of a rational number greater than 1.
+        Compute square root of reciprocal (or approximation).
 
-        Computes sqrt such that ``sqrt**2 == self.inverse``,
-        or up to desired accuracy if not a perfect square.
-
-        params
-        + num_digits : int
-
-        return
-        Rational
+        + num_digits: int
+        ~> Rational
         """
         if self._numerator <= self._denominator:
             raise ValueError('Inverse square root only if greater than 1')
