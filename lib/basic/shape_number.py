79a7a951f9d5cfc5915aae3b28fcdc63d488070e
itscomputers <its_computers@protonmail.com>
2021-11-14 12:55:15 -0700

    add typing to basic/shape_numbers

--- lib/basic/shape_number.py
+++ lib/basic/shape_number.py
@@ -2,35 +2,33 @@
 #   - module for functions related to shape numbers
 
 # ===========================================================
+from typing import Optional
+
 from .division import div
 from .sqrt import integer_sqrt, is_square
 
 # ===========================================================
-__all__ = [
-    "shape_number_by_index",
-    "which_shape_number",
-]
-# ===========================================================
 
 
-def shape_number_by_index(k, sides):
+def shape_number_by_index(index: int, sides: int) -> int:
     """
-    Compute the `k`th polygonal shape number, given number of `sides`.
+    Compute the `index`th polygonal shape number, given number of `sides`.
 
     example: `shape_number_by_index(5, 3) ~> 15`
         since 15 is the 5th triangular number
-
-    + k: int --at least 0
-    + sides: int --at least 3
-    ~> int
     """
-    return k * (k * (sides - 2) - sides + 4) // 2
+    if index < 0:
+        raise ValueError("index must be positive")
+    if sides < 3:
+        raise ValueError("polygon needs at least 3 sides")
+
+    return index * (index * (sides - 2) - sides + 4) // 2
 
 
 # =============================
 
 
-def which_shape_number(number, sides):
+def which_shape_number(number: int, sides: int) -> Optional[int]:
     """
     Compute, if possible, shape number index for `number` with given number
     of `sides`. Inverse of `lib.basic.shape_number.shape_number_by_index`.
@@ -38,10 +36,6 @@ def which_shape_number(number, sides):
     examples: `which_shape_number(15, 3) ~> 5`
               `which_shape_number(20, 3) ~> None`
                 since 20 is not triangular number
-
-    + number: int
-    + sides: int --at least 3
-    ~> int
     """
     denom = 2 * (sides - 2)
     root = 8 * (sides - 2) * number + (sides - 4) ** 2
@@ -52,3 +46,5 @@ def which_shape_number(number, sides):
     q, r = div(sides - 4 + integer_sqrt(root), denom)
     if r == 0:
         return q
+
+    return None
