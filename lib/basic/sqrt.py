25605c3fe1f48ec6bba18b33dc078b5b5b24b081
itscomputers <its_computers@protonmail.com>
2021-11-14 12:51:10 -0700

    add typing to basic/sqrt

--- lib/basic/sqrt.py
+++ lib/basic/sqrt.py
@@ -5,22 +5,14 @@
 import math
 
 # ===========================================================
-__all__ = [
-    "integer_sqrt",
-    "is_square",
-]
-# ===========================================================
 
 
-def integer_sqrt(number, guess=None):
+def integer_sqrt(number: int, guess: int = None) -> int:
     """
     Compute the integer part of the square root of `number`.
 
     example: `integer_sqrt(30) ~> 5`
         since `5**2 == 25 <= 30 < 36 == 6**2`
-
-    + number: int --at least 0
-    ~> int
     """
     if guess is None:
         guess = int(math.sqrt(number))
@@ -34,15 +26,12 @@ def integer_sqrt(number, guess=None):
 # =============================
 
 
-def is_square(number):
+def is_square(number: int) -> bool:
     """
     Determine whether `number` is a perfect square.
 
     examples: `is_square(25) ~> True`
               `is_square(30) ~> False`
               `is_square(-25) ~> False`
-
-    + number: int
-    ~> bool
     """
     return number >= 0 and integer_sqrt(number) ** 2 == number
