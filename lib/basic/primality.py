16237b74fe0f854bb4eeb7d84d348ad549e4bc7f
itscomputers <its_computers@protonmail.com>
2021-11-14 12:49:01 -0700

    add typing to basic/primality

--- lib/basic/primality.py
+++ lib/basic/primality.py
@@ -3,26 +3,20 @@
 
 # ===========================================================
 import itertools as it
+from typing import Iterator, List
 
 from .sqrt import integer_sqrt
 
 # ===========================================================
-__all__ = [
-    "prime_gen",
-    "iter_primes_up_to",
-    "primes_up_to",
-    "is_prime__naive",
-]
-# ===========================================================
 
 
-def prime_gen():
+def prime_gen() -> Iterator[int]:
     """
     Generator that yields prime numbers using Sieve of Eratosthenes.
 
     ~> Iterator[int]
     """
-    primes = it.count(2)
+    primes: Iterator[int] = it.count(2)
     while True:
         prime = next(primes)
         primes = filter(prime.__rmod__, primes)
@@ -32,14 +26,11 @@ def prime_gen():
 # -----------------------------
 
 
-def iter_primes_up_to(number):
+def iter_primes_up_to(number: int) -> Iterator[int]:
     """
     Iterator of primes up to `number`.
 
     example: `list(iter_primes_up_to(10)) ~> [2, 3, 5, 7]`
-
-    + number: int
-    ~> Iterator[int]
     """
     return it.takewhile(lambda x: x <= number, prime_gen())
 
@@ -47,19 +38,15 @@ def iter_primes_up_to(number):
 # -----------------------------
 
 
-def primes_up_to(max_value, primes=None, numbers_left=None):
+def primes_up_to(
+    max_value: int, primes: List[int] = [], numbers_left: Iterator[int] = None
+) -> List[int]:
     """
     Compute primes up to `max_value`.
 
     example: `primes_up_to(19) ~> [2, 3, 5, 7, 11, 13, 17, 19]`
-
-    + max_value: int
-    + primes: List[int] --used for recursive call
-    + numbers_left: Iterator[int] --used for recursive call
-    ~> List[int]
     """
-    if primes is None and numbers_left is None:
-        primes = []
+    if numbers_left is None:
         numbers_left = iter(range(2, max_value + 1))
 
     try:
@@ -78,16 +65,12 @@ def primes_up_to(max_value, primes=None, numbers_left=None):
 # =============================
 
 
-def is_prime__naive(number, numbers_left=None):
+def is_prime__naive(number: int, numbers_left: Iterator[int] = None) -> bool:
     """
     Determine if `number` is prime using sieve of Eratosthenes.
 
     example: `is_prime__naive(19) ~> True`
              `is_prime__naive(20) ~> False`
-
-    + number: int
-    + numbers_left: Iterator[int] --used for recursive call
-    ~> bool
     """
     if number < 2:
         return False
